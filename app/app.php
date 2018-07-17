fdf8c290b898a1c27d193d2b683bfd528da1bf86
Tijmen Wierenga <twieren0@xs4all.net>
2018-07-17 13:24:17 +0200

    Integrated Redis for storing count

--- app/app.php
+++ app/app.php
@@ -2,11 +2,22 @@
 require_once 'vendor/autoload.php';
 
 $loop = React\EventLoop\Factory::create();
+
+$redis = new \Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => 'redis',
+    'port'   => 6379,
+]);
+
+$total = (int) $redis->get("count");
+
 $counter = 0;
 
-$loop->addPeriodicTimer(1, function () use (&$counter, $loop) {
+$loop->addPeriodicTimer(1, function () use (&$counter, $loop, $redis, $total) {
     $counter++;
-    echo $counter . PHP_EOL;
+    echo $total + $counter . PHP_EOL;
+
+    $redis->incr("count");
 
     if ($counter >= 10) {
         $loop->stop();
